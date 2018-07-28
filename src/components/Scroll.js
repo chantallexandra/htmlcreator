e7101c54ead330b5e58d6b49e28bb449082a4226
chantallexandra <c.montgomery@rogers.com>
2018-07-28 12:30:00 -0400

    Scroll functionality within div

--- src/components/Scroll.js
+++ src/components/Scroll.js
@@ -4,7 +4,6 @@ import React from 'react';
 
 const Scroll = (props) => {
 	// props.children is the elements within the scroll tag
-	// Must use camelCase for css in JSX
 	return(
 		<div id='scroll'> 
 			{props.children}

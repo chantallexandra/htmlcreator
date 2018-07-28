020ba7f6702849fbf31bb84c11f9b8dd2f51d1b8
chantallexandra <c.montgomery@rogers.com>
2018-07-28 12:31:49 -0400

    Added blue style

--- src/components/selection/selection.js
+++ src/components/selection/selection.js
@@ -4,12 +4,13 @@ import './selection.css';
 const Selection = ({select}) => {
 
 	return(
-		<div className='center selection-bar'>
+		<div className='center selection-bar header'>
 			<p className="para-inline">I want to create a...</p>
 			<select onChange={select}>
 				<option value="default"></option>
 				<option value="gradient">Background Gradient</option>
 				<option value="table">Table</option>
+				<option value="div">Align Content</option>
 			</select>
 		</div>
 	)
